2176a5e68698a8930e1a843676229925e0070b83
Drevoed <vetrokm@gmail.com>
2020-07-15 21:56:40 +0300

    Draft zero-copy implementation

--- src/config.rs
+++ src/config.rs
@@ -1,45 +1,47 @@
 use serde::Deserialize;
 
 #[derive(Deserialize)]
-pub struct Config {
+pub struct Config<'a> {
     pub debug: bool,
     pub timeout: u32,
     pub multiclient_limit: u8,
     pub max_chars: u32,
     pub zalgo_tolerance: u8,
-    pub general: GeneralConfig,
-    pub masterserver: MasterServerConfig,
+    #[serde(borrow)]
+    pub general: GeneralConfig<'a>,
+    #[serde(borrow)]
+    pub masterserver: MasterServerConfig<'a>,
     pub wtce_floodguard: FloodGuardConfig,
-    pub music_change_floodguard: FloodGuardConfig,
+    pub music_change_floodguard: FloodGuardConfig
 }
 
 #[derive(Deserialize)]
-pub struct GeneralConfig {
-    pub hostname: String,
+pub struct GeneralConfig<'a> {
+    pub hostname: &'a str,
     pub playerlimit: u8,
     pub port: u32,
     pub local: bool,
-    pub modpass: String,
-    pub motd: String,
+    pub modpass: &'a str,
+    pub motd: &'a str,
     pub use_websockets: bool,
     pub websocket_port: u32,
 }
 
 #[derive(Deserialize)]
-pub struct MasterServerConfig {
+pub struct MasterServerConfig<'a> {
     #[serde(rename = "use")]
     pub use_masterserver: bool,
-    pub ip: String,
+    pub ip: &'a str,
     pub port: u32,
-    pub name: String,
-    pub description: String,
+    pub name: &'a str,
+    pub description: &'a str,
 }
 
 #[derive(Deserialize)]
 pub struct FloodGuardConfig {
     pub times_per_interval: u8,
     pub interval_length: u8,
-    pub mute_length: u32,
+    pub mute_length: u32
 }
 
 #[cfg(test)]
@@ -48,8 +50,41 @@ mod tests {
 
     #[test]
     fn test_config_parsing() {
-        let config_str =
-            std::fs::read_to_string("./config/config.toml").unwrap();
+        let config_str = r#"
+        debug = false
+        timeout = 250
+        multiclient_limit = 16
+        max_chars = 256
+        zalgo_tolerance = 3
+        
+        
+        [general]
+        hostname = "<dollar>H"
+        playerlimit = 100
+        port = 27016
+        local = false
+        modpass = "mod"
+        motd = "Welcome to my server!"
+        use_websockets = true
+        websocket_port = 50001
+        
+        [masterserver]
+        use = true
+        ip = "master.aceattorneyonline.com"
+        port = 27016
+        name = "My server"
+        description = "My server description!"
+        
+        [music_change_floodguard]
+        times_per_interval = 3
+        interval_length = 20
+        mute_length = 180
+        
+        [wtce_floodguard]
+        times_per_interval = 5
+        interval_length = 10
+        mute_length = 1000
+        "#;
         let config: Config = toml::from_str(&config_str).unwrap();
 
         assert_eq!(config.debug, false);
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 #![allow(unused)]
 use log::LevelFilter;
-use rusttorney::{server::AOServer};
+use rusttorney::{server::AOServer, config::Config};
 use env_logger::Env;
+use std::path::PathBuf;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    env_logger::from_env(Env::default().default_filter_or("info")).init();
+    let filter: &str;
 
-    AOServer::new()?.run().await
+    let config_path = PathBuf::from("./config/config.toml");
+    let config_string = std::fs::read_to_string(&config_path)?;
+    let config: Config = toml::from_str(&config_string)?;
+
+    if config.debug {
+        filter = "debug"
+    } else {
+        filter = "info"
+    }
+
+
+    env_logger::from_env(Env::default().default_filter_or(filter)).init();
+
+    AOServer::new(config)?.run().await
 }
